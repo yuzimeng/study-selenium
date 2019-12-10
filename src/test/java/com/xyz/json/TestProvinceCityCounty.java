756f25c58aa01ec8508f8004f9ba08aee35ef99a
rain <xinyuxyz@hotmail.com>
2019-12-10 23:30:02 +0800

    province test

--- src/test/java/com/xyz/json/TestProvinceCityCounty.java
+++ src/test/java/com/xyz/json/TestProvinceCityCounty.java
@@ -9,25 +9,28 @@ public class TestProvinceCityCounty {
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		List<Map<String, Object>> dataList = ProvinceCityCountyDataUtils.getProvinceCityCountyData();
-		for (Map<String, Object> province : dataList) {
-			String provinceName = (String) province.get("name");
-			String provinceCode = (String) province.get("code");
-			System.out.println(provinceCode + " : " + provinceName);
-			List<Map<String, Object>> cityList = (List<Map<String, Object>>) province.get("cityList");
-			for (Map<String,Object> city : cityList) {
-				String cityName = (String) city.get("name");
-				String cityCode = (String) city.get("code");
-				System.out.println(cityCode + " : " + cityName);
-				List<Map<String, Object>> areaList = (List<Map<String, Object>>) city.get("areaList");
-				for (Map<String,Object> area : areaList) {
-					String countyName = (String) area.get("name");
-					String countyCode = (String) area.get("code");
-					System.out.println(countyCode + " : " + countyName);
-					
-				}
-			}
-		}
+//		List<Map<String, Object>> dataList = ProvinceCityCountyDataUtils.getProvinceCityCountyData();
+//		for (Map<String, Object> province : dataList) {
+//			String provinceName = (String) province.get("name");
+//			String provinceCode = (String) province.get("code");
+//			System.out.println(provinceCode + " : " + provinceName);
+//			List<Map<String, Object>> cityList = (List<Map<String, Object>>) province.get("cityList");
+//			for (Map<String,Object> city : cityList) {
+//				String cityName = (String) city.get("name");
+//				String cityCode = (String) city.get("code");
+//				System.out.println(cityCode + " : " + cityName);
+//				List<Map<String, Object>> areaList = (List<Map<String, Object>>) city.get("areaList");
+//				for (Map<String,Object> area : areaList) {
+//					String countyName = (String) area.get("name");
+//					String countyCode = (String) area.get("code");
+//					System.out.println(countyCode + " : " + countyName);
+//					
+//				}
+//			}
+//		}
+		String str = "长春市农安县朝阳路200号";
+		System.out.println(ProvinceCityCountyDataUtils.addressResolution(str));
+		
 
 	}
 
